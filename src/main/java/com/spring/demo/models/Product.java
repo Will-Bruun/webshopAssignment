dcce09a27a01e75450b8018851e2595b64b2dbe2
Will-Bruun <75360110+Will-Bruun@users.noreply.github.com>
2021-03-07 19:27:15 +0100

    Basic products
    Simple gets and sets for the product model according to common standard.

--- src/main/java/com/spring/demo/models/Product.java
+++ src/main/java/com/spring/demo/models/Product.java
@@ -1,5 +1,41 @@
 package com.spring.demo.models;
 
 public class Product {
-    
+
+    private String ID;
+    private String Name;
+    private Integer Price;
+    private Integer Stock;
+
+    public String getID(){
+        return ID;
+    }
+
+    public void setID(String ID){
+        this.ID = ID;
+    }
+
+    public String getName(){
+        return Name;
+    }
+
+    public void setName(String name){
+        this.Name = name;
+    }
+
+    public Integer getPrice(){
+        return Price;
+    }
+
+    public void setPrice(Integer price) {
+        this.Price = price;
+    }
+
+    public Integer getStock(){
+        return Stock;
+    }
+
+    public void setStock(Integer stock){
+        this.Stock = stock;
+    }
 }
