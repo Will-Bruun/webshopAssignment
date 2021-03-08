ae1793f9454e17f874697713c218200ee5c6244a
Will-Bruun <75360110+Will-Bruun@users.noreply.github.com>
2021-03-08 09:06:28 +0100

    Added barebones controller
    Added a bareboned product controller, it currently has one get route, and one health check route.

--- src/main/java/com/spring/demo/controllers/ProductController.java
+++ src/main/java/com/spring/demo/controllers/ProductController.java
@@ -1,13 +1,23 @@
 package com.spring.demo.controllers;
 
 import com.spring.demo.exceptions.ProductNotFoundException;
+import com.spring.demo.models.Product;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.*;
 
+@RestController
 public class ProductController {
 
+    @RequestMapping("/rest")
+    public String healthCheck() {
+        return "OK";
+    }
 
-
+    @RequestMapping("/products/get")
+    public Product getProduct(@RequestParam(value = "id", defaultValue = "Unknown") String id){
+        Product product = new Product();
+        product.setID(id);
+        return product;
+    }
 
 }
