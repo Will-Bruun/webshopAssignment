f9262e2d11f451e981d807d033c2fc9cc6f3a2e1
Will-Bruun <75360110+Will-Bruun@users.noreply.github.com>
2021-03-08 10:53:22 +0100

    Added CRD to controller
    Created two read paths, one delete path, and one create path for ProductController. As of now no update path exists.
    It's using a product repo that's autowired as a base.
    Commented out section is attempts at getting a read all path in.

--- src/main/java/com/spring/demo/controllers/ProductController.java
+++ src/main/java/com/spring/demo/controllers/ProductController.java
@@ -2,22 +2,47 @@ package com.spring.demo.controllers;
 
 import com.spring.demo.exceptions.ProductNotFoundException;
 import com.spring.demo.models.Product;
+import com.spring.demo.repositories.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 public class ProductController {
+
+    @Autowired
+    private ProductRepository repo;
 
     @RequestMapping("/rest")
     public String healthCheck() {
         return "OK";
     }
 
-    @RequestMapping("/products/get")
-    public Product getProduct(@RequestParam(value = "id", defaultValue = "Unknown") String id){
-        Product product = new Product();
-        product.setID(id);
-        return product;
+    @GetMapping("/products/get/{id}")
+    public Product getProduct(@PathVariable(value = "id") String id){
+        return repo.retrieve(id);
     }
 
+    @GetMapping("/products/search/{name}")
+    public Product searchProduct(@PathVariable("name") String name){
+        return repo.search(name);
+    }
+
+    @PostMapping("/products/create")
+    public Product createProduct(@RequestBody Product prod){
+        repo.store(prod);
+        return prod;
+    }
+
+    @DeleteMapping("/products/delete/{id}")
+    public Product deleteProduct(@PathVariable("id") String id){
+        return repo.delete(id);
+    }
+
+    //@GetMapping("/products/getAll")
+    //public List<Product> getAllProducts(){
+      //  return repo.getAll();
+    //}
 }
