13147a841e11664a89503553fe657da5fb6c0ad5
Will-Bruun <75360110+Will-Bruun@users.noreply.github.com>
2021-04-12 10:06:40 +0200

    Acidentally put some services in controllers
    Managed to put services under the controller file structure. This and two coming comits are to make sure everything is in it's right place.

--- src/main/java/com/spring/demo/services/ShoppingService.java
+++ src/main/java/com/spring/demo/services/ShoppingService.java
@@ -1,4 +1,20 @@
 package com.spring.demo.services;
 
+import com.spring.demo.models.Delivery;
+import com.spring.demo.models.ShoppingCart;
+
 public class ShoppingService {
+
+    public static Delivery createDelivery(String adress, ShoppingCart cart){
+        Delivery deliv = new Delivery();
+        deliv.setDeliveryAddress(adress);
+        deliv.setShoppingCart(cart);
+
+        return deliv;
+    }
+
+    public static ShoppingCart connectDelivery(ShoppingCart cart, Delivery deliv){
+        cart.appendDelivery(deliv);
+        return cart;
+    }
 }
