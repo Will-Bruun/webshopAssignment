78a9ae030c7f6970eefc0e496e73383daac385c9
Will-Bruun <75360110+Will-Bruun@users.noreply.github.com>
2021-04-14 13:51:26 +0200

    Added payment repository

---
+++ src/main/java/com/spring/demo/repositories/PaymentRepository.java
@@ -0,0 +1,15 @@
+package com.spring.demo.repositories;
+
+import com.spring.demo.models.Delivery;
+import com.spring.demo.models.Payment;
+import com.spring.demo.models.Shipment;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface PaymentRepository extends CrudRepository<Payment, String> {
+
+    List<Payment> getPaymentsFromDeliveries(List<Delivery> deliveries);
+
+    List<Payment> getPaymentsFromShipments(List<Shipment> shipments);
+}
